b9b189ce7b66cd96fc4268283e09319e9336445d
amira654 <samy75960@gmail.com>
2025-04-19 15:46:00 +0200

    finish details screen

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,36 @@
+import 'package:bookly/core/utils/style.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton(
+      {super.key,
+      required this.backGroundColor,
+      required this.textColor,
+      required this.text,
+      this.borderRadius});
+
+  final Color backGroundColor;
+  final Color textColor;
+  final String text;
+  final BorderRadius? borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 48,
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+            backgroundColor: backGroundColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: borderRadius ?? BorderRadius.circular(12),
+            )),
+        child: Text(
+          text,
+          style: Styles.textStyle18
+              .copyWith(color: textColor, fontWeight: FontWeight.w600),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/book_rating.dart
+++ lib/features/home/presentation/views/widgets/book_rating.dart
@@ -4,30 +4,37 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../../../core/utils/style.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating(
+      {super.key, this.mainAxisAlignment = MainAxisAlignment.start});
+
+  final MainAxisAlignment mainAxisAlignment;
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: [
-        const Icon(
+      mainAxisAlignment: mainAxisAlignment,
+      children: const [
+        Icon(
           FontAwesomeIcons.solidStar,
           color: Color(0xFFFFDD4F),
-          size: 18,
+          size: 14,
         ),
-        const SizedBox(
+        SizedBox(
           width: 6,
         ),
-        const Text(
+        Text(
           "4.8",
           style: Styles.textStyle16,
         ),
-        const SizedBox(
+        SizedBox(
           width: 5,
         ),
-        Text(
-          "(245)",
-          style: Styles.textStyle14.copyWith(color: const Color(0xFF707070)),
+        Opacity(
+          opacity: 0.5,
+          child: Text(
+            "(245)",
+            style: Styles.textStyle14,
+          ),
         ),
       ],
     );
